4b1f176c31b419038b49f003adc1a010a7e39d4b
Adella <adellakenn@gmail.com>
2019-06-06 00:18:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
-# class Song 
-#   attr_accessor :title 
+class Song 
+  attr_accessor :title 
   
-#   def initialize(title)
-#     @title = title
-#   end
+  def initialize(title)
+    @title = title
+  end
   
 
-# end
+end
