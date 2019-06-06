15e064d60bd131c059f5417923e5e3c79d68e066
Adella <adellakenn@gmail.com>
2019-06-06 00:13:43 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,7 +2,7 @@ class Post
   attr_accessor :title, :author
   
   def author=(author)
-    author.class 
+    author.class      # puts Author 
     if author.is_a?(Author)
       @author = author
     end
