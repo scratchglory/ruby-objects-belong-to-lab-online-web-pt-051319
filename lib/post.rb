978d9f8b7ec6824be85c45d7d38665e02328f074
Adella <adellakenn@gmail.com>
2019-06-06 00:14:09 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,16 @@
+require 'pry'
+
 class Post 
   attr_accessor :title, :author
   
   def author=(author)
     author.class      # puts Author 
+    
     if author.is_a?(Author)
       @author = author
+    binding.pry
     end
+    
     
   end
   
